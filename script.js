9a656c3e6343efebbdc1143772013836af2b57bd
Maria667e <maria.pedroso.calixto@escola.pr.gov.br>
2025-06-26 11:44:28 +0000

    att 26/06

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(Button => {
     Button.addEventListener('click', function(){
-       const atual = document.querySelector('.ative')
+       const atual = document.querySelector('.ativo')
        const proximoPasso = 'passo-' + this.getAttribute ('data-proximo');
 
        atual.classList.remove('ativo');
